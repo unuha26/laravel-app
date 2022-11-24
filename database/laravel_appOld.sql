d24295178d022b55926a17b2ada4c8dae61894ad
unuha26 <unuha26@ymail.com>
2022-11-24 17:20:02 +0700

    Update Database

--- database/laravel_appOld.sql
+++ database/laravel_appOld.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Nov 2022 pada 11.18
+-- Waktu pembuatan: 04 Nov 2022 pada 06.08
 -- Versi server: 10.4.25-MariaDB
 -- Versi PHP: 8.0.23
 
@@ -48,19 +48,20 @@ INSERT INTO `crane` (`id_crane`, `crane`) VALUES
 
 CREATE TABLE `data_part` (
   `id` int(10) NOT NULL,
-  `kode_part` varchar(15) NOT NULL,
-  `nama_part` varchar(100) NOT NULL,
-  `lokasi` varchar(20) NOT NULL,
+  `id_part` int(10) NOT NULL,
+  `kondisi_part` varchar(50) NOT NULL,
+  `id_lokasi` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `status_part` varchar(100) NOT NULL
+  `detail` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data untuk tabel `data_part`
 --
 
-INSERT INTO `data_part` (`id`, `kode_part`, `nama_part`, `lokasi`, `jumlah`, `status_part`) VALUES
-(2, '123123', 'qweqw', '1', 1, 'wqe');
+INSERT INTO `data_part` (`id`, `id_part`, `kondisi_part`, `id_lokasi`, `jumlah`, `detail`) VALUES
+(1, 1, 'Baik , Baru', 1, 10, 'Bagus bngt ges'),
+(2, 2, 'Baik, Bekas', 2, 12, 'Bekas seh mantep');
 
 -- --------------------------------------------------------
 
@@ -88,7 +89,7 @@ INSERT INTO `divisi` (`id_divisi`, `divisi`) VALUES
 --
 
 CREATE TABLE `grup` (
-  `id` int(10) NOT NULL,
+  `id_grup` int(10) NOT NULL,
   `grup` varchar(10) NOT NULL,
   `tanggal` date NOT NULL,
   `shift` varchar(10) NOT NULL
@@ -98,7 +99,7 @@ CREATE TABLE `grup` (
 -- Dumping data untuk tabel `grup`
 --
 
-INSERT INTO `grup` (`id`, `grup`, `tanggal`, `shift`) VALUES
+INSERT INTO `grup` (`id_grup`, `grup`, `tanggal`, `shift`) VALUES
 (1, 'A', '2022-09-01', '1'),
 (2, 'B', '2022-09-02', '2'),
 (3, 'C', '2022-09-03', '3'),
@@ -176,36 +177,74 @@ CREATE TABLE `mekanik` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `part`
+--
+
+CREATE TABLE `part` (
+  `id_part` int(10) NOT NULL,
+  `nama_part` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `part`
+--
+
+INSERT INTO `part` (`id_part`, `nama_part`) VALUES
+(2, 'Cable Reels');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `part_lama`
+--
+
+CREATE TABLE `part_lama` (
+  `id_part_lama` int(10) NOT NULL,
+  `part_lama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data untuk tabel `part_lama`
+--
+
+INSERT INTO `part_lama` (`id_part_lama`, `part_lama`) VALUES
+(3, 'Cable Reels'),
+(4, 'Beacon');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `riwayat`
 --
 
 CREATE TABLE `riwayat` (
   `id` int(10) NOT NULL,
   `id_part` int(10) NOT NULL,
+  `id_part_lama` int(10) NOT NULL,
+  `nama_part` varchar(50) NOT NULL,
   `tanggal` date NOT NULL,
-  `tanggal_selesai` date NOT NULL,
+  `shift` int(10) NOT NULL,
   `id_crane` int(11) NOT NULL,
+  `id_user` int(10) NOT NULL,
   `id_grup` int(10) NOT NULL,
-  `id_users` int(11) NOT NULL,
-  `keterangan` text NOT NULL,
-  `status` varchar(50) NOT NULL
+  `keterangan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data untuk tabel `riwayat`
 --
 
-INSERT INTO `riwayat` (`id`, `id_part`, `tanggal`, `tanggal_selesai`, `id_crane`, `id_grup`, `id_users`, `keterangan`, `status`) VALUES
-(1, 2, '2022-09-01', '0000-00-00', 2, 2, 1, 'Trouble', 'Antrian');
+INSERT INTO `riwayat` (`id`, `id_part`, `id_part_lama`, `nama_part`, `tanggal`, `shift`, `id_crane`, `id_user`, `id_grup`, `keterangan`) VALUES
+(1, 2, 3, 'Laser Scanner', '2022-09-01', 1, 2, 1, 2, 'Trouble');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Struktur dari tabel `user`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE `user` (
+  `id` int(4) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(300) NOT NULL,
   `nik` varchar(20) NOT NULL,
@@ -219,20 +258,16 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `nik`, `nama`, `id_jabatan`, `id_divisi`, `role`, `create_at`, `update_at`, `register_by`) VALUES
+INSERT INTO `user` (`id`, `username`, `password`, `nik`, `nama`, `id_jabatan`, `id_divisi`, `role`, `create_at`, `update_at`, `register_by`) VALUES
 (1, 'Eko Aji', '81dc9bdb52d04dc20036dbd8313ed055', '1633423443234', 'Eko Aji', 1, 2, 'admin', '2022-09-14 14:25:39', '2022-11-04 03:25:24', NULL),
 (5, 'uuuu', 'werwerwerewr', '1092340921830', 'Fajar', 1, 1, 'admin', '2022-11-04 03:56:07', '2022-11-04 03:56:07', ''),
 (6, 'werweir', '0394oihjfewi908', '09238402348', 'Jarrr', 2, 2, 'admin', '2022-11-04 03:56:37', '2022-11-04 03:56:37', ''),
 (7, 'ijok', '980u9ewcdo8', '023984', 'Ulin Nuha', 1, 2, 'admin', '2022-11-04 03:57:02', '2022-11-04 03:57:02', ''),
 (8, 'test', '$2y$10$1/Lm6JhWLWouT1YwOf.d6OQmp1rJXgaVsY6pMqs34uR41xSWjJv9q', '0932i4023', 'faja', 1, 2, 'admin', '2022-11-04 04:28:55', '2022-11-04 04:28:55', ''),
-(9, 'aaaaaaa', '$2y$10$DImf1Sr2cyZtFUSruJpj1eTqWN.6HsAO6hURqBMkW3raSC50e0FIG', '111', '111', 1, 1, '1', '2022-11-04 04:29:18', '2022-11-04 04:29:18', ''),
-(10, '12345', '$2y$10$hHJSzvKyNEoaJR1dAckWGewq3U4gdVtTgD73oiJa0cMzUWUXj87xG', '12345', '12345', 1, 1, 'admin', '2022-11-04 11:34:16', '2022-11-04 11:34:16', ''),
-(11, '1234', '1234', '1232131232133', '1234', 1, 1, '1', '2022-11-23 02:31:25', '2022-11-23 02:31:25', ''),
-(12, '111111', '11', '16332432143211', 'Ulin Nuha1', 2, 2, '1', '2022-11-23 02:42:25', '2022-11-23 02:42:56', NULL),
-(13, 'asdsadasd', '$2y$10$B8FBVV64lYSWmxPvMy92teLmuP0EGJQg9Rr.v4PMPwgDlxfWSCfA.', '16332432143212', 'asdasdsad', 2, 2, '1', '2022-11-23 04:47:01', '2022-11-23 04:47:01', '');
+(9, 'aaaaaaa', '$2y$10$DImf1Sr2cyZtFUSruJpj1eTqWN.6HsAO6hURqBMkW3raSC50e0FIG', '111', '111', 1, 1, '1', '2022-11-04 04:29:18', '2022-11-04 04:29:18', '');
 
 --
 -- Indexes for dumped tables
@@ -248,7 +283,10 @@ ALTER TABLE `crane`
 -- Indeks untuk tabel `data_part`
 --
 ALTER TABLE `data_part`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_part` (`id_part`),
+  ADD KEY `id_lokasi` (`id_lokasi`),
+  ADD KEY `id_part_2` (`id_part`);
 
 --
 -- Indeks untuk tabel `divisi`
@@ -260,7 +298,7 @@ ALTER TABLE `divisi`
 -- Indeks untuk tabel `grup`
 --
 ALTER TABLE `grup`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_grup`);
 
 --
 -- Indeks untuk tabel `gudang`
@@ -291,19 +329,32 @@ ALTER TABLE `mekanik`
   ADD KEY `id_part` (`id_part`);
 
 --
+-- Indeks untuk tabel `part`
+--
+ALTER TABLE `part`
+  ADD PRIMARY KEY (`id_part`);
+
+--
+-- Indeks untuk tabel `part_lama`
+--
+ALTER TABLE `part_lama`
+  ADD PRIMARY KEY (`id_part_lama`);
+
+--
 -- Indeks untuk tabel `riwayat`
 --
 ALTER TABLE `riwayat`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_part` (`id_part`),
+  ADD KEY `id_part_lama` (`id_part_lama`),
   ADD KEY `id_crane` (`id_crane`),
-  ADD KEY `id_grup` (`id_grup`),
-  ADD KEY `user` (`id_users`);
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_grup` (`id_grup`);
 
 --
--- Indeks untuk tabel `users`
+-- Indeks untuk tabel `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_jabatan` (`id_jabatan`),
   ADD KEY `id_divisi` (`id_divisi`);
@@ -322,7 +373,7 @@ ALTER TABLE `crane`
 -- AUTO_INCREMENT untuk tabel `data_part`
 --
 ALTER TABLE `data_part`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `divisi`
@@ -334,7 +385,7 @@ ALTER TABLE `divisi`
 -- AUTO_INCREMENT untuk tabel `grup`
 --
 ALTER TABLE `grup`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_grup` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `gudang`
@@ -361,40 +412,66 @@ ALTER TABLE `mekanik`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `part`
+--
+ALTER TABLE `part`
+  MODIFY `id_part` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `part_lama`
+--
+ALTER TABLE `part_lama`
+  MODIFY `id_part_lama` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT untuk tabel `riwayat`
 --
 ALTER TABLE `riwayat`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT untuk tabel `user`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+ALTER TABLE `user`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
+-- Ketidakleluasaan untuk tabel `data_part`
+--
+ALTER TABLE `data_part`
+  ADD CONSTRAINT `data_part_ibfk_1` FOREIGN KEY (`id_lokasi`) REFERENCES `lokasi` (`id_lokasi`);
+
+--
 -- Ketidakleluasaan untuk tabel `gudang`
 --
 ALTER TABLE `gudang`
-  ADD CONSTRAINT `gudang_ibfk_1` FOREIGN KEY (`id_grup`) REFERENCES `grup` (`id`);
+  ADD CONSTRAINT `gudang_ibfk_1` FOREIGN KEY (`id_grup`) REFERENCES `grup` (`id_grup`);
+
+--
+-- Ketidakleluasaan untuk tabel `mekanik`
+--
+ALTER TABLE `mekanik`
+  ADD CONSTRAINT `mekanik_ibfk_1` FOREIGN KEY (`id_part`) REFERENCES `part` (`id_part`);
 
 --
 -- Ketidakleluasaan untuk tabel `riwayat`
 --
 ALTER TABLE `riwayat`
+  ADD CONSTRAINT `riwayat_ibfk_1` FOREIGN KEY (`id_part_lama`) REFERENCES `part_lama` (`id_part_lama`),
+  ADD CONSTRAINT `riwayat_ibfk_2` FOREIGN KEY (`id_part`) REFERENCES `part` (`id_part`),
   ADD CONSTRAINT `riwayat_ibfk_3` FOREIGN KEY (`id_crane`) REFERENCES `crane` (`id_crane`),
-  ADD CONSTRAINT `riwayat_ibfk_6` FOREIGN KEY (`id_grup`) REFERENCES `grup` (`id`);
+  ADD CONSTRAINT `riwayat_ibfk_4` FOREIGN KEY (`id_grup`) REFERENCES `grup` (`id_grup`);
 
 --
--- Ketidakleluasaan untuk tabel `users`
+-- Ketidakleluasaan untuk tabel `user`
 --
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_divisi`) REFERENCES `divisi` (`id_divisi`),
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`);
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_divisi`) REFERENCES `divisi` (`id_divisi`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
